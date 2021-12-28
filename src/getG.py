07ac6d0d52ea708a8c0d33d39c32626848b6fec5
roee-tal <roeetal979@gmail.com>
2021-12-28 19:34:25 +0200

    final

--- src/getG.py
+++
@@ -1,9 +0,0 @@
-from src.GraphAlgo import GraphAlgo
-
-
-def get_g(file_name: str):
-    g = GraphAlgo()
-    if g.load_from_json(file_name) is True:
-        ans = g.get_graph()
-        return ans
-    return null
